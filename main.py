1eb982432f51f2a1b12dd692262c3a616291f308
Yashsonaar <sonaryash1406@gmail.com>
2024-04-10 13:47:35 +0530

    Pre-trained sentiment analysis model code added

---
+++ main.py
@@ -0,0 +1,18 @@
+from customtwitter import APIX
+import pandas as pd
+import json
+
+if __name__ == '__main__':
+    APIkey = "05f9eec068mshcb0f7471140a0a8p1ca951jsn9d87189f621b"
+    
+    inst = APIX(key = APIkey)
+
+    obj = inst.searchTweet("BTC", search_type = "top")
+
+    # print(json.dumps(obj, indent = 4))
+    # with open("latest_tweet_replies.txt", "w+") as f:
+    #     f.write(json.dumps(obj, indent = 4))
+    json_string = json.dumps(obj)
+
+    df = pd.read_json(json_string)
+    print(df)
